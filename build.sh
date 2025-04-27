ec8e23e79bc621c501d5413c511dfb7ab23f5449
Diego Simoncini <simoncinidiego10@gmail.com>
2025-04-27 11:46:04 +0200

    d

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 chmod +x ./node_modules/.bin/vite
 chmod +x ./node_modules/vite/bin/vite.js
-node ./node_modules/vite/bin/vite.js build 
+VITE_BASE_URL=./ node ./node_modules/vite/bin/vite.js build 
